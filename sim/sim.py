c8e2e90864f77af4402558e5448ddf1152a225c0
valeria <valeria.detomas@mail.polimi.it>
2023-08-25 12:31:25 +0200

    small update

--- sim/sim.py
+++ sim/sim.py
@@ -2,33 +2,32 @@ import subprocess
 import os
 import shutil
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from itertools import product
 import re
 
 # Define the range for the varying parameters
-NUM_PIECES_range = range(20, 40, 2)
-PROCESSING_MEAN_S2_range = range(50, 500, 20)
-SPEED_range = range(5, 30, 5)
+NUM_PIECES_range = range(5, 30, 6)
+PROCESSING_MEAN_S2_range = range(10,500, 100)
+ERROR_PROB_OUTS2_range = range(1, 180, 50)
 
 not_satisfied = []
 
 # Define the fixed parameters
-NUM_SLOTS = 32
-GATE_POS = -1
+SPEED = 5
+NUM_SLOTS = 61
+GATE_POS = -1 #there is no gate in these configurations
 FIRST_POS_BELT2 = -1
 LAST_POS_BELT2 = -1
 RULE_CONTROLLER = 10
 PROCESSING_MEAN_S1 = 10
 PROCESSING_VAR_S1 = 2
-PROCESSING_VAR_S2 = 2
+PROCESSING_VAR_S2 = 5
 PROCESSING_MEAN_S3 = 10
 PROCESSING_VAR_S3 = 2
 ERROR_PROB_S1 = 1
 ERROR_PROB_S2 = 1
 ERROR_PROB_S3 = 1
 ERROR_PROB_OUTS1 = 1
-ERROR_PROB_OUTS2 = 1
 ERROR_PROB_OUTS3 = 1
 
 NUM_STATIONS = 3
@@ -38,16 +37,16 @@ NUM_OUT_SENSORS = 3
 # Initialize lists to store results for plotting
 NUM_PIECES_results = []
 PROCESSING_MEAN_S2_results = []
-SPEED_results = []
-deadlock_results = []
+ERROR_PROB_OUTS2_results = []
+query_results = []
 
 # Loop through all combinations of varying parameters
-for NUM_PIECES, PROCESSING_MEAN_S2, SPEED in product(NUM_PIECES_range, PROCESSING_MEAN_S2_range, SPEED_range):
+for NUM_PIECES, PROCESSING_MEAN_S2, ERROR_PROB_OUTS2 in product(NUM_PIECES_range, PROCESSING_MEAN_S2_range, ERROR_PROB_OUTS2_range):
     # Define the parameters string for the UPPAAL model
 
     # Create a temporary model file with the new parameters
     original_model_path = "conf.xml"
-    temp_model_path = f"temp_model_{NUM_PIECES}_{PROCESSING_MEAN_S2}_{SPEED}.xml"
+    temp_model_path = f"temp_model_{NUM_PIECES}_{PROCESSING_MEAN_S2}_{ERROR_PROB_OUTS2}.xml"
     shutil.copyfile(original_model_path, temp_model_path)
 
     with open(temp_model_path, "r") as f:
@@ -83,29 +82,38 @@ for NUM_PIECES, PROCESSING_MEAN_S2, SPEED in product(NUM_PIECES_range, PROCESSIN
     
     # Run UPPAAL model with verifyta
     #verifyta_path = "/Applications/uppaal64-4.1.26/bin-Darwin/verifyta" # Path to the verifyta executable
-    command = f'./verifyta -s -o2 -a 0.01 -B 0.01 -E 0.01 "{temp_model_path}"' # Comando verifyta
+    command = f'/Applications/uppaal64-4.1.26/bin-Darwin/verifyta -s -o2 -a 0.01 -B 0.01 -E 0.01 {temp_model_path}' # Comando verifyta
     result = os.popen(command).read()  # Execute the command and capture its output
 
     lines = result.split('\n')  # Split the output into lines
 
-
-    print("NUM_PIECES:", NUM_PIECES, " - ", "PROCESSING_MEAN_S2:", PROCESSING_MEAN_S2, " - ", "SPEED:", SPEED)
+    print("NUM_PIECES:", NUM_PIECES, " - ", "PROCESSING_MEAN_S2:", PROCESSING_MEAN_S2, " - ", "ERROR_PROB_OUTS2:", ERROR_PROB_OUTS2)
     pattern = r'\[(.*?),(.*)\]'
     match = re.search(pattern, lines[9])
-    print("probability of deadlock: ", match.group(2))
-     
+    #print("probability of not deadlock: ", match.group(1), match.group(2))
+    integers = re.findall(r'\[([\d.]+),([\d.]+)\]', lines[9])
+    # Convert the extracted strings to float numbers
+    integers = [(float(match[0]), float(match[1])) for match in integers]
+    # Print the extracted integers
+
+    # Calculate the average for each pair of integers
+    averages = [(pair[0] + pair[1]) / 2 for pair in integers]
+
+    print (averages)
+
     os.remove(temp_model_path)
 
     NUM_PIECES_results.append(NUM_PIECES)
     PROCESSING_MEAN_S2_results.append(PROCESSING_MEAN_S2)
-    SPEED_results.append(SPEED)
+    ERROR_PROB_OUTS2_results.append(ERROR_PROB_OUTS2)
+    query_results.append(averages)
 
 # Plot the results in a 3D scatter plot
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-sc = ax.scatter(NUM_PIECES_results, PROCESSING_MEAN_S2_results, SPEED_results, c=not_satisfied, cmap='viridis')
+ax = fig.add_subplot(111, projection='3d', proj_type = 'ortho')
+sc = ax.scatter(NUM_PIECES_results, PROCESSING_MEAN_S2_results, ERROR_PROB_OUTS2_results, c=query_results, cmap='coolwarm')
 plt.colorbar(sc)
 ax.set_xlabel('NUM_PIECES')
 ax.set_ylabel('PROCESSING_MEAN_S2')
-ax.set_zlabel('SPEED')
+ax.set_zlabel('ERROR_PROB_OUTS2')
 plt.show()
